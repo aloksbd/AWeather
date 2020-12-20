7df75b7f2e97de6404d94bcf31cb7eca3c91008a
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-20 10:38:40 +0545

    Delivers error .connectivity if client returns error

--- AWeatherTests/ARemoteForecastLoaderTests.swift
+++ AWeatherTests/ARemoteForecastLoaderTests.swift
@@ -12,20 +12,30 @@ class ARemoteForecastLoader{
     private var url: URL
     private var httpClient: HTTPClientSpy
     
+    enum Error: Swift.Error{
+        case connectivity
+    }
+    
     init(httpClient: HTTPClientSpy, url: URL){
         self.httpClient = httpClient
         self.url = url
     }
     
-    func load(){
-        httpClient.get(from: url)
+    func load(completion: @escaping (Error) -> ()){
+        httpClient.get(from: url){ error in
+            completion(.connectivity)
+        }
     }
 }
 
 class HTTPClientSpy{
     var requestedUrls = [URL]()
+    var error: Error?
     
-    func get(from url: URL){
+    func get(from url: URL,completion: @escaping (Error) -> ()) {
+        if let error = error{
+            completion(error)
+        }
         requestedUrls.append(url)
     }
 }
@@ -43,22 +53,34 @@ class ARemoteForecastLoaderTests: XCTestCase {
         let url = URL(string: "https://url.com")!
         let (sut,client) = makeSUT(url: url)
         
-        sut.load()
+        sut.load{_ in}
         XCTAssertNotNil(client.requestedUrls)
     }
     
     func test_load_requestDataFromUrlTwice(){
         let url = URL(string: "https://url.com")!
         let (sut,client) = makeSUT(url: url)
-
-        sut.load()
-        sut.load()
+        
+        sut.load{_ in}
+        sut.load{_ in}
         XCTAssertEqual(client.requestedUrls, [url,url])
+    }
+    
+    func test_load_deliversErrorOnClientError(){
+        let (sut,client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: ARemoteForecastLoader.Error?
+        sut.load{ error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
     }
     
     //MARK: Helpers
     
-    private func makeSUT(url: URL) -> (sut: ARemoteForecastLoader, client: HTTPClientSpy){
+    private func makeSUT(url: URL = URL(string: "https://url.com")!) -> (sut: ARemoteForecastLoader, client: HTTPClientSpy){
         let client = HTTPClientSpy()
         let sut = ARemoteForecastLoader(httpClient: client, url: url)
         return (sut,client)
