79f33f724efdca32df8f9b3b895e6091fc8c908e
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-20 20:50:14 +0545

    Create a helper group and move makeItem to its own file

---
+++ AWeatherTests/Helpers/AForecastItemMaker.swift
@@ -0,0 +1,67 @@
+//
+//  AForecastItemMaker.swift
+//  AWeatherTests
+//
+//  Created by Alok Subedi on 20/12/2020.
+//
+
+import Foundation
+@testable import AWeather
+
+func makeItem() -> (item: AForecast, json: [String : Any]){
+    let weather = Weather(main: "sunny")
+    let weatherJSON = [
+        "main": weather.main
+    ]
+    
+    let temperature = Temperature(
+        min: "12",
+        max: "12",
+        day: "12",
+        night: "12",
+        morn: "12",
+        eve: "12")
+    let temperatureJSON = [
+        "min": temperature.min,
+        "max": temperature.max,
+        "day": temperature.day,
+        "night": temperature.night,
+        "morn": temperature.morn,
+        "eve": temperature.eve
+    ]
+    
+    let dailyForecast = ADailyForecast(
+        dt: "1234543",
+        sunrise: "1235543",
+        sunset: "1234123",
+        humidity: "123",
+        speed: "12",
+        pop: "12%",
+        snow: "32",
+        weather: weather,
+        temperature: temperature
+    )
+    let dailyForecastJSON = [
+        "dt": dailyForecast.dt,
+        "sunrise": dailyForecast.sunrise,
+        "sunset": dailyForecast.sunset,
+        "humidity": dailyForecast.humidity ?? "",
+        "speed": dailyForecast.speed ?? "",
+        "pop": dailyForecast.pop ?? "",
+        "snow": dailyForecast.snow ?? "",
+        "weather": weatherJSON,
+        "temperature": temperatureJSON
+    ] as [String : Any]
+    
+    let city = City(name: "Ktm")
+    let cityJSON = ["name": city.name]
+    
+    let forecastItem = AForecast(city: city, list: [dailyForecast])
+    let forecastItemJSON = [
+        "city": cityJSON,
+        "list": [dailyForecastJSON]
+    ] as [String : Any]
+    
+    return(forecastItem, forecastItemJSON)
+}
+
