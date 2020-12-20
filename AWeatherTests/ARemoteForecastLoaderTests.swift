101b1cc843cd965db010c92780211dd7bf94451a
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-20 08:11:04 +0545

    Initialize ARemoteForecastLoader with HTTPClientSpy

---
+++ AWeatherTests/ARemoteForecastLoaderTests.swift
@@ -0,0 +1,31 @@
+//
+//  ARemoteForecastLoaderTests.swift
+//  AWeatherTests
+//
+//  Created by Alok Subedi on 19/12/2020.
+//
+
+import XCTest
+@testable import AWeather
+
+class ARemoteForecastLoader{
+    private var httpClient: HTTPClientSpy
+    
+    init(httpClient: HTTPClientSpy){
+        self.httpClient = httpClient
+    }
+}
+
+class HTTPClientSpy{
+    var requestedUrls = [URL]()
+}
+
+class ARemoteForecastLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromUrl(){
+        let httpClient = HTTPClientSpy()
+        _ = ARemoteForecastLoader(httpClient: httpClient)
+        
+        XCTAssertTrue(httpClient.requestedUrls.isEmpty)
+    }
+}
