2fd2667fe7cb8d108fd76ed48de615b4b5c1febb
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-20 21:35:30 +0545

    add memory leak detection

--- AWeatherTests/ARemoteForecastLoaderTests.swift
+++ AWeatherTests/ARemoteForecastLoaderTests.swift
@@ -150,9 +150,12 @@ class ARemoteForecastLoaderTests: XCTestCase {
     
     //MARK: Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://url.com")!) -> (sut: ARemoteForecastLoader, client: HTTPClientSpy){
+    private func makeSUT(url: URL = URL(string: "https://url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: ARemoteForecastLoader, client: HTTPClientSpy){
         let client = HTTPClientSpy()
         let sut = ARemoteForecastLoader(httpClient: client, url: url)
+        
+        trackForMemoryLeak(client, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
         return (sut,client)
     }
     
@@ -174,62 +177,12 @@ class ARemoteForecastLoaderTests: XCTestCase {
         action()
         wait(for: [exp], timeout: 1.0)
     }
-    
-    func makeItem() -> (item: AForecast, json: [String : Any]){
-        
-        let weather = Weather(main: "sunny")
-        let weatherJSON = [
-            "main": weather.main
-        ]
-        
-        let temperature = Temperature(
-            min: "12",
-            max: "12",
-            day: "12",
-            night: "12",
-            morn: "12",
-            eve: "12")
-        let temperatureJSON = [
-            "min": temperature.min,
-            "max": temperature.max,
-            "day": temperature.day,
-            "night": temperature.night,
-            "morn": temperature.morn,
-            "eve": temperature.eve
-        ]
-        
-        let dailyForecast = ADailyForecast(
-            dt: "1234543",
-            sunrise: "1235543",
-            sunset: "1234123",
-            humidity: "123",
-            speed: "12",
-            pop: "12%",
-            snow: "32",
-            weather: weather,
-            temperature: temperature
-        )
-        let dailyForecastJSON = [
-            "dt": dailyForecast.dt,
-            "sunrise": dailyForecast.sunrise,
-            "sunset": dailyForecast.sunset,
-            "humidity": dailyForecast.humidity ?? "",
-            "speed": dailyForecast.speed ?? "",
-            "pop": dailyForecast.pop ?? "",
-            "snow": dailyForecast.snow ?? "",
-            "weather": weatherJSON,
-            "temperature": temperatureJSON
-        ] as [String : Any]
-        
-        let city = City(name: "Ktm")
-        let cityJSON = ["name": city.name]
-        
-        let forecastItem = AForecast(city: city, list: [dailyForecast])
-        let forecastItemJSON = [
-            "city": cityJSON,
-            "list": [dailyForecastJSON]
-        ] as [String : Any]
-        
-        return(forecastItem, forecastItemJSON)
+}
+
+extension XCTestCase{
+    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line){
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have deallocated. Possible memory leak",file: file, line: line)
+        }
     }
 }
