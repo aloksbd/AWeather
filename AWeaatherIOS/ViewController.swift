59f34ac90771cd170fc11ef9ff59c9f2fc6ff163
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-21 20:41:06 +0545

    add ios app target to project

---
+++ AWeaatherIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWeaatherIOS
+//
+//  Created by Alok Subedi on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
