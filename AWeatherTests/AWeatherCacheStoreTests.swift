052f2852dcc954331ee7005dbb98cadde0ac40d0
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-21 14:57:07 +0545

    AWeatherCacheStore Delivers nil on empty cache and inserted value after insertion

---
+++ AWeatherTests/AWeatherCacheStoreTests.swift
@@ -0,0 +1,74 @@
+//
+//  AWeatherCacheStoreTests.swift
+//  AWeatherTests
+//
+//  Created by Alok Subedi on 21/12/2020.
+//
+
+import XCTest
+import AWeather
+
+class AWeatherCacheStore{
+    private let CACHE = "cache"
+    
+    func retrieve(completion: @escaping (CacheStore.RetrievalResult) -> ()){
+        guard let data = UserDefaults.standard.data(forKey: CACHE) else {
+            return completion(.success(nil))
+        }
+        let cacheForecast = try! JSONDecoder().decode(AForecast.self, from: data)
+        completion(.success(cacheForecast))
+        
+    }
+    
+    func insert(_ item: AForecast, completion: @escaping CacheStore.InsertionCompletion){
+        guard let data = try? JSONEncoder().encode(item) else {
+            return
+        }
+        UserDefaults.standard.setValue(data, forKey: CACHE)
+        completion(nil)
+    }
+}
+
+class AWeatherCacheStoreTests: XCTestCase {
+
+    func test_retrieve_deliversNilOnEmptyCache(){
+        let sut = AWeatherCacheStore()
+        
+        let exp = expectation(description: "wait for retrieval block")
+        
+        sut.retrieve { result in
+            switch result{
+            case .failure(_):
+                XCTFail("expected nil")
+            case let .success(forecast):
+                XCTAssertNil(forecast)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValue(){
+        let sut = AWeatherCacheStore()
+        let item = forecastItem()
+
+        let exp = expectation(description: "wait for retrieval block")
+
+        sut.insert(item) { insertionError in
+            XCTAssertNil(insertionError, "expected feed to be inserted successfully")
+            sut.retrieve { retrieveResult in
+                switch (retrieveResult){
+                case let .success(forecast):
+                    XCTAssertEqual(forecast, item)
+                default:
+                    XCTFail("expected success with \(item)")
+                }
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
