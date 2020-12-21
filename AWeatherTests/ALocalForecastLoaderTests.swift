3197f63ff029521d2ee2e92048adc0646529b67d
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-12-21 10:51:11 +0545

    Should not save new cache on deletion error

--- AWeatherTests/ALocalForecastLoaderTests.swift
+++ AWeatherTests/ALocalForecastLoaderTests.swift
@@ -20,32 +20,58 @@ class ALocalForecastLoader{
 }
 
 class CacheStore{
-    var deletCacheCallCount = 0
+    var deleteCacheCallCount = 0
+    var insertCallCount = 0
     
     func deleteCacheFeed(){
-        deletCacheCallCount += 1
+        deleteCacheCallCount += 1
+    }
+    
+    func deletion(with error: NSError,at index: Int = 0){
+        
     }
 }
 
 class ALocalForecastLoaderTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation(){
-        let store = CacheStore()
-        _ = ALocalForecastLoader(store: store)
+        let (_, store) = makeSUT()
         
-        XCTAssertEqual(store.deletCacheCallCount, 0)
+        XCTAssertEqual(store.deleteCacheCallCount, 0)
     }
     
     func test_save_requestsCacheDeletion(){
-        let store = CacheStore()
-        let sut = ALocalForecastLoader(store: store)
+        let (sut, store) = makeSUT()
         let item = forecastItem()
         sut.save(item)
-        XCTAssertEqual(store.deletCacheCallCount, 1)
+        XCTAssertEqual(store.deleteCacheCallCount, 1)
+    }
+    
+    func test_save_doesNotRequestsCacheInsertionOnDeletionError(){
+        let (sut, store) = makeSUT()
+        let item = forecastItem()
+        let deletionError = anyNSError()
+        
+        sut.save(item)
+        store.deletion(with: deletionError)
+        XCTAssertEqual(store.insertCallCount, 0)
     }
     
     //MARK: helpers
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut:ALocalForecastLoader, store: CacheStore){
+        let store = CacheStore()
+        let sut = ALocalForecastLoader(store: store)
+        
+        trackForMemoryLeak(store,file: file, line: line)
+        trackForMemoryLeak(sut,file: file, line: line)
+        
+        return (sut,store)
+    }
     
     func forecastItem() -> AForecast{
         return makeItem().item
+    }
+    
+    func anyNSError() -> NSError{
+        return NSError(domain: "any error", code: 0)
     }
 }
